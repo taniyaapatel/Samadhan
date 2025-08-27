ebb1d4b937dacb71dd730c889ea1e44c51699824
taniyaapatel <taniyaa2004@gmail.com>
2025-08-28 00:22:31 +0530

    hiiiiiiii

--- temp/public/script.js
+++
@@ -1,6 +0,0 @@
-// Just a little animation effect
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-    alert(`You clicked on ${card.innerText}`);
-  });
-});
